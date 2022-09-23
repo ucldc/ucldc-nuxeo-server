c11711c902ffd518debb21be9f02fcf090e09560
Barbara Hui <barbara.hui@ucop.edu>
2022-09-23 10:51:41 -0700

    Add argparse for env and version

--- create_ucldc_conf.py
+++ create_ucldc_conf.py
@@ -1,4 +1,5 @@
 import sys, os
+import argparse
 import boto3
 import json
 from jinja2 import Environment, FileSystemLoader
@@ -6,8 +7,9 @@ from jinja2 import Environment, FileSystemLoader
 '''
 Create ucldc.conf file using values from AWS Parameter Store
 '''
-def main():
-    param_path = '/nuxeo/2021-prod/nuxeo_conf'
+def main(params):
+    param_path = f"/nuxeo/{params.version}-{params.env}/nuxeo_conf"
+    print(f"{param_path=}")
     session = boto3.Session(region_name='us-west-2')
     ssm = session.client('ssm')
 
@@ -21,20 +23,6 @@ def main():
     param_dict = {}
     for p in params:
         param_dict[p['Name']] = p['Value']
-
-    #print(param_dict)
-
-    '''
-    nuxeo_url = param_dict[f'{param_path}/nuxeo_url']
-    nuxeo_db_password = param_dict[f'{param_path}/nuxeo_db_password']
-    nuxeo_db_host = param_dict[f'{param_path}/nuxeo_db_host']
-    nuxeo_redis_host = param_dict[f'{param_path}/nuxeo_redis_host']
-    elasticsearch_address_list = param_dict[f'{param_path}/elasticsearch_address_list']
-    nuxeo_s3storage_bucket = param_dict[f'{param_path}/nuxeo_s3storage_bucket']
-    nuxeo_s3storage_region = param_dict[f'{param_path}/nuxeo_s3storage_region']
-    mail_transport_user = param_dict[f'{param_path}/mail_transport_user']
-    mail_transport_password = param_dict[f'{param_path}/mail_transport_password']
-    '''
 
     environment = Environment(loader=FileSystemLoader("templates/"))
     template = environment.get_template('ucldc.conf.template')
@@ -55,4 +43,8 @@ def main():
         f.write(content)
 
 if __name__ == "__main__":
-    sys.exit(main())
+    parser = argparse.ArgumentParser()
+    parser.add_argument('env', choices=['prod', 'stg'])
+    parser.add_argument('version', choices=['2021'])
+    args = parser.parse_args()
+    sys.exit(main(args))

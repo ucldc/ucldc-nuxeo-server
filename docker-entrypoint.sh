8a35d8b5b0ff6f811c3e2be46876057e3390a13a
Barbara Hui <barbara.hui@ucop.edu>
2025-02-06 15:55:41 -0800

    Add SKIP_CONFIG env var to docker-entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,12 @@
 set -e
 
 # run a python script to create config file at /etc/nuxeo/nuxeo.conf
-python3 /client_config/configure.py
+if [[ ${SKIP_CONFIG} != true ]] ; then
+    if ! python3 /client_config/configure.py; then
+        echo "Error running /client_config/configure.py"
+        exit 1
+    fi
+fi
 
 # run Nuxeo's docker entrypoint script
 exec /docker-entrypoint.sh "$@"

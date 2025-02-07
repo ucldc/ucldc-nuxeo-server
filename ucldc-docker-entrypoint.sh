0f7df1de8a51297c81840da10dbc0af4f234628b
Barbara Hui <barbara.hui@ucop.edu>
2025-02-06 16:46:31 -0800

    Namespace env var

--- ucldc-docker-entrypoint.sh
+++ ucldc-docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # run a python script to create config file at /etc/nuxeo/nuxeo.conf
-if [[ ${SKIP_CONFIG} != true ]] ; then
+if [[ ${NUXEO_SKIP_CONFIG} != true ]] ; then
     if ! python3 /client_config/configure.py; then
         echo "Error running /client_config/configure.py"
         exit 1
